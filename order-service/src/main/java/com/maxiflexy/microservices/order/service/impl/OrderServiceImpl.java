e8fb2bb991fdb98e16a850246f7fbd44ef6e45e7
Maxiflexy <onyemax247@gmail.com>
2024-10-05 17:41:16 +0100

    created order integration test

--- order-service/src/main/java/com/maxiflexy/microservices/order/service/impl/OrderServiceImpl.java
+++ order-service/src/main/java/com/maxiflexy/microservices/order/service/impl/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package com.maxiflexy.microservices.order.service.impl;
 
+import com.maxiflexy.microservices.order.client.InventoryClient;
 import com.maxiflexy.microservices.order.dto.OrderRequest;
 import com.maxiflexy.microservices.order.model.Order;
 import com.maxiflexy.microservices.order.repository.OrderRepository;
@@ -14,16 +15,23 @@ import java.util.UUID;
 public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest){
-        // map OrderRequest to Order object
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
 
-        orderRepository.save(order);
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+        if(isProductInStock){
+            // map OrderRequest to Order object
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+
+            orderRepository.save(order);
+        }else {
+            throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in Stock");
+        }
 
     }
 }
