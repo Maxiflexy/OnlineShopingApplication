8cc50e5fe95acc25ec4f9bf6fdb36dc310fb9b3f
Maxiflexy <onyemax247@gmail.com>
2024-10-05 07:30:25 +0100

    created inventory controller

---
+++ inventory-service/src/main/java/com/maxiflexy/microservices/inventory/controller/InventoryController.java
@@ -0,0 +1,20 @@
+package com.maxiflexy.microservices.inventory.controller;
+
+import com.maxiflexy.microservices.inventory.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity){
+        return inventoryService.isInStock(skuCode, quantity);
+    }
+}
